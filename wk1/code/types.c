d3dac11d678a141ed6ec0ce318d44cae5f73f50a
Ian Wienand <ian@wienand.org>
2006-11-14 04:35:42 +0000

    types illustration

---
+++ wk1/code/types.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void)
+{
+	char a;
+	char *p = "hello";
+
+	int i;
+
+	// moving a larger variable into a smaller one
+	i = 0x12341234;
+	a = i;
+	i = a;
+	printf("i is %d\n", i);
+
+	// moving a pointer into an integer
+	printf("p is %p\n", p);
+	i = p;
+	// "fooling" with casts
+	i = (int)p;
+	p = (char*)i;
+	printf("p is %p\n", p);
+
+	return 0;
+}
